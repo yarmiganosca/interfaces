16718f69737f53e51295e3af585eacdcc62908f2
Chris Hoffman <choffman@optoro.com>
2014-08-17 02:09:01 -0400

    Improved the tests

--- test/interfaces_spec.rb
+++ test/interfaces_spec.rb
@@ -1,44 +1,39 @@
 require_relative './spec_helper'
 
 describe Interfaces do
-  describe 'Kernel#interface' do
+  interface :Thingable do
+    requires :thingy
 
-    interface :Thingable do
-      requires :thingy
-
-      def whatsit
-        thingy + 4
-      end
-    end
-
-    it 'creates a constant with the interface name' do
-      Object.const_defined?(:Thingable, true).must_equal true
+    def whatsit
+      thingy + 4
     end
   end
 
-  describe 'Class#implement' do
-    class A
-      def thingy
-        2
-      end
+  it 'creates a constant with the interface name' do
+    Object.const_defined?(:Thingable, true).must_equal true
+  end
 
-      implement Thingable
+  class A
+    def thingy
+      2
     end
 
-    class B
-      implement Thingable
-    end
+    implement Thingable
+  end
 
-    it "extends the implementing class with the interface's methods" do
-      A.new.must_respond_to :whatsit
-    end
+  it "extends the implementing class with the interface's methods" do
+    A.new.must_respond_to :whatsit
+  end
 
-    it 'the methods return the correct results' do
-      A.new.whatsit.must_equal 6
-    end
+  it 'the methods return the correct results' do
+    A.new.whatsit.must_equal 6
+  end
 
-    it "doesn't extend the implementing class if the required methods aren't defined" do
-      B.new.wont_respond_to :whatsit
-    end
+  class B
+    implement Thingable
+  end
+
+  it "doesn't extend the implementing class if the required methods aren't defined" do
+    B.new.wont_respond_to :whatsit
   end
 end
